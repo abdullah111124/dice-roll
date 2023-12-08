7d7bd97269b1df6caee28920563caa845a2ede68
abdullah111124 <145787244+abdullah111124@users.noreply.github.com>
2023-12-08 09:37:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+// Dice Roll Simulator Demo
+
+// HTML Element for Output
+let outputEl = document.getElementById("output");
+
+// Menu Button
+document.getElementById("go-btn").addEventListener("click", goBtnClicked);
+
+function goBtnClicked() {
+  // Get Menu Selection
+  let selection = document.getElementById("menu").value;
+
+  // Process Selection
+  if (selection === "roll-once") {
+    rollOnce();
+  } else if (selection === "roll-five") {
+    roll5Times();
+  } else if (selection === "roll-n") {
+    rollNTimes();
+  } else if (selection === "snake-eyes") {
+    snakeEyes();
+  }
+}
+
+function rollOnce() {
+  // Display Menu Title
+  outputEl.innerHTML = "<h3>ROLL ONCE</h3>";
+
+  // Random Dice Roll & Total
+  let die1 = Math.floor(Math.random() * 6) + 1;
+  let die2 = Math.floor(Math.random() * 6) + 1;
+  let total = die1 + die2;
+
+  // Add Results to output element
+  let pEl = document.createElement("p");
+  pEl.innerHTML = `${die1},${die2} (sum: ${total})`;
+  outputEl.appendChild(pEl);
+}
+
+function roll5Times() {
+  // Display Menu Title
+  outputEl.innerHTML = "<h3>ROLL 5 TIMES</h3>";
+
+  // Random Dice Roll & Total
+  for (loops = 0; loops < 5; loops++) {
+    let die1 = Math.floor(Math.random() * 6) + 1;
+    let die2 = Math.floor(Math.random() * 6) + 1;
+    let total = die1 + die2;
+    // Add Results to output element
+    let pEl = document.createElement("p");
+    pEl.innerHTML = `${die1},${die2} (sum: ${total})`;
+    outputEl.appendChild(pEl);
+  }
+}
+
+function rollNTimes() {
+  // Display Menu Title
+  outputEl.innerHTML = "<h3>ROLL 'N' TIMES</h3>";
+  let Numrolls = +prompt("please enter a  number");
+
+  for (let loops = 0; loops < Numrolls; loops++) {
+    let die1 = Math.floor(Math.random() * 6) + 1;
+    let die2 = Math.floor(Math.random() * 6) + 1;
+    let total = die1 + die2;
+
+    // Add Results to output element
+    let pEl = document.createElement("p");
+    pEl.innerHTML = `${die1},${die2} (sum: ${total})`;
+    outputEl.appendChild(pEl);
+  }
+}
+
+function snakeEyes() {
+  // Display Menu Title
+  outputEl.innerHTML = "<h3>ROLL UNTIL SNAKE EYES</h3>";
+
+  let countvar = 0;
+  while (true) {
+    countvar++;
+
+    // Random Dice Roll
+    let die1 = Math.floor(Math.random() * 6) + 1;
+    let die2 = Math.floor(Math.random() * 6) + 1;
+    let total = die1 + die2;
+    let pEl = document.createElement("p");
+    pEl.innerHTML = `${die1},${die2} (sum: ${total})`;
+    outputEl.appendChild(pEl);
+
+    // STOP THE LOOP IF SNAKE EYES
+    if (total === 2) {
+      break;
+    }
+  }
+  // Output Snake Eyes
+  document.getElementById("snake eyes").innerHTML = `${+countvar}`;
+}
+// let password = prompt ("enter a password")
+// let confirm password = prompt ("confirm your password")
+// if(password === check password){alert}
